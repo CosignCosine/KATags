11b943d6bf6a895597d1d037f03ed084d0e6a84d
Caleb Mayhew <blazeprograms@gmail.com>
2018-01-30 11:56:07 -0500

    0.0.11

--- inject.js
+++ inject.js
@@ -14,27 +14,56 @@ if(sh === undefined || sh === null){
     localStorage.setItem('cosigncosine-show', false);
 }
 var pr = new RegExp(pl.join("|"), "gim");
-console.warn(pr)
+
+var ft = true; //first time runnning?
+
+function ext(s){
+  switch(s){
+
+    // cs discussion
+    case 0:
+      // add block button next to each comment post
+      var discussion = document.querySelectorAll('.discussion-meta-controls');
+      Array.prototype.forEach.call(discussion, function(el){
+        if(el.querySelectorAll('.cosigncosine-block').length === 0 && !el.parentNode.parentNode.parentNode.className.match(/voting\-wrap/gim)){
+          // discussion-meta-separator
+          var sp = document.createElement("span")
+          sp.innerHTML = ("• ");
+          sp.className = "discussion-meta-separator";
+          el.appendChild(sp);
+
+          // block button
+          var bl = document.createElement("span");
+          bl.innerHTML = " <a href='javascript:void(0);' class='cosigncosine-block' title='Block this user'>Block</a>";
+          bl.className = "flag-tools";
+          el.appendChild(bl);
+        }
+      });
+
+      if(ft){
+        var blocks = document.querySelectorAll('.cosigncosine-block');
+        Array.prototype.forEach.call(discussion, function(el){
+          el.addEventListener("click", function(event){
+            var user = el.parentNode.parentNode.parentNode.querySelectorAll('.discussion-meta-info')[0].querySelectorAll('.author-nickname')[0].textContent.replace("View profile for: ", "").trim().replace(/\!|\||\\|\?|\!|\*|\+|\{|\}|\(|\)|\[|\]|\^|\$|\&/gim, "");
+            if(confirm('Are you sure that you want to block ' + user + '?')){
+              console.warn('blocked ' + user)
+              pl.push(user);
+              pr = new RegExp(pl.join("|"), "gim");
+              localStorage.setItem("cosigncosine-block", JSON.stringify(pl));
+            }
+          })
+        })
+      }
+
+      ft = false;
+    break;
+
+  }
+}
 
 var int = setInterval(function(){
   if(document.querySelectorAll('.discussion-meta-controls').length > 0){
-    // add block button next to each comment post
-    var discussion = document.querySelectorAll('.discussion-meta-controls');
-    Array.prototype.forEach.call(discussion, function(el, i){
-      if(el.querySelectorAll('.cosigncosine-block').length === 0){
-        // discussion-meta-separator
-        var sp = document.createElement("span")
-        sp.innerHTML = ("• ");
-        sp.className = "discussion-meta-separator";
-        el.appendChild(sp);
-
-        // block button
-        var bl = document.createElement("span");
-        bl.innerHTML = " <a href='javascript:void(0);' class='cosigncosine-block' title='Block this user'>Block</a>";
-        bl.className = "flag-tools";
-        el.appendChild(bl)
-      }
-    });
+    ext(0);
   }
   // add block button on profile
 
